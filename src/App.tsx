49097cf092fc276764c516ebc639fa4fefa1aa40
marlevilla <cmarlenevt@hotmail.com>
2025-03-27 19:34:58 -0600

    nuevo carusel  infinto

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,21 @@ import Carrusel from "./Carrusel";
 import Infinito from "./CarruselInfinito";
 
 import Cards from './components/Cards';
-import Cards2 from './components/Cards2';
-import Cards3 from './components/Cards3';
+
 import Cards4 from './components/Cards4';
+import { cardimg1, cardimg2, cardimg3 } from './lib/constants/card.imgs';
 
 
-const diapositivas: React.ReactNode[] = [<Cards />, <Cards2 />, <Cards3 />];
+const diapositivas: React.ReactNode[] = [<Cards imgs={cardimg1} />, <Cards imgs={cardimg2} />, <Cards imgs={cardimg3} />];
 const diapositivas2: React.ReactNode[] = [<Cards4 />];
 
 
 const App: React.FC = () => {
   return (
     <>
-      <h1 className='text-5xl text-red-300'>Holla!</h1>
 
-
-    {/*  <Carrusel slides={diapositivas} />*/}
       <Infinito slides={diapositivas2} />
+      <Carrusel slides={diapositivas} title='Carusel React' />
 
 
     </>
