15f07b42ea600731b9ff222f3192d0a385ad8f79
Jackson avila paez <arenca44@gmail.com>
2025-03-28 01:55:38 -0500

    carrusel infinito ok!

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,19 @@ import { cardimg4 } from './lib/constants/card.imgs';
 
 // const diapositivas: React.ReactNode[] = [<Cards imgs={cardimg1} />, <Cards imgs={cardimg2} />, <Cards imgs={cardimg3} />];
 
-
-
+const descriptions: string[] = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec.',
+];
 const App: React.FC = () => {
   return (
     <>
 
-      <Infinito slides={cardimg4} />
-
+      <Infinito slides={cardimg4} descriptions={descriptions} />
 
       {/* <Carrusel slides={diapositivas} title='Carusel React' /> */}
-
 
     </>
 
--- src/CarruselInfinito.tsx
+++ src/CarruselInfinito.tsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
 
-
 interface CarouselProps {
     autoSlide?: boolean;
     autoSlideInterval?: number;
-    slides: React.ReactNode[];
+    descriptions: string[];
+    slides: string[];
 }
 
-const CarruselInfinito: React.FC<CarouselProps> = ({
+const CarruselInfinito = ({
     slides,
+    descriptions,
     autoSlide = true,
-    autoSlideInterval = 3000,
-}) => {
+    autoSlideInterval = 4000,
+}: CarouselProps) => {
+
     const [curr, setCurr] = useState(0);
 
-    // const prev = () =>
-    //     setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
     const next = () =>
         setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
 
@@ -26,33 +26,31 @@ const CarruselInfinito: React.FC<CarouselProps> = ({
     }, [autoSlide, autoSlideInterval, curr]);
 
     return (
-        <div className="overflow-hidden relative w-[500px] h-[300px] mx-auto rounded-lg shadow-lg bg-gray-800">
+        <div className="relative w-full">
 
-            <div className="flex justify-center items-center overflow-hidden">
-                <div
-                    className="flex transition-transform ease-in-out duration-700 "
-                    style={{ transform: `translateX(-${curr * 100}%)` }}
-                >
-                    {slides.map((slide, index) => (
-                        <div key={index} className="min-w-full flex-shrink-0  w-full">
-                            <img className="w-full" src={`${slide}`} alt="" />
-                        </div>
-                    ))}
-
-
+            <div className="overflow-hidden w-full h-[500px]">
+                <div className="w-full h-full flex items-center justify-center rounded-[20px] overflow-hidden">
+                    <div
+                        className="flex transition-transform ease-in-out duration-700 w-full h-full"
+                        style={{ transform: `translateX(-${curr * 100}%)` }}
+                    >
+                        {slides.map((slide, index) => (
+                            <div key={index} className="relative flex-shrink-0 w-full h-full">
+                                <img className="w-full h-full object-cover" src={slide} alt={`Slide ${index}`} />
+                                <div className="absolute left-[50%] bottom-[50%] transform -translate-x-1/2 w-full p-4 ">
+                                    <p className="text-white">{descriptions[index]}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-
-
-
-            {/* Indicadores */}
-            <div className="absolute bottom-4 right-0 left-0 flex items-center justify-center gap-2">
-
+            {/* Puntos de navegación */}
+            <div className="absolute left-[50%] bottom-2 transform -translate-x-1/2 flex items-center justify-center gap-2">
                 {slides.map((_, i) => (
                     <div
                         key={i}
-                        className={`w-3 h-3 bg-white rounded-full transition-all ${curr === i ? "p-2" : "bg-opacity-50"
-                            }`}
+                        className={`transition-all w-3 h-3 bg-gray-500 rounded-full ${curr === i ? "bg-lime-200 bg-opacity-100" : "bg-opacity-50"}`}
                     />
                 ))}
             </div>
