985462199e0cdf23ea18a85878a418f99b77f169
Jackson avila paez <arenca44@gmail.com>
2025-03-27 21:04:15 -0500

    se actualiza infinito carrusel

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-
-
 import './App.css'
 import React from "react";
-import Carrusel from "./Carrusel";
+// import Carrusel from "./Carrusel";
 import Infinito from "./CarruselInfinito";
 
-import Cards from './components/Cards';
-
-import Cards4 from './components/Cards4';
-import { cardimg1, cardimg2, cardimg3 } from './lib/constants/card.imgs';
+// import Cards from './components/Cards';
 
 
-const diapositivas: React.ReactNode[] = [<Cards imgs={cardimg1} />, <Cards imgs={cardimg2} />, <Cards imgs={cardimg3} />];
-const diapositivas2: React.ReactNode[] = [<Cards4 />];
+import { cardimg4 } from './lib/constants/card.imgs';
+// import { cardimg1, cardimg2, cardimg3 } from './lib/constants/card.imgs';
+
+
+// const diapositivas: React.ReactNode[] = [<Cards imgs={cardimg1} />, <Cards imgs={cardimg2} />, <Cards imgs={cardimg3} />];
+
 
 
 const App: React.FC = () => {
   return (
     <>
 
-      <Infinito slides={diapositivas2} />
-      <Carrusel slides={diapositivas} title='Carusel React' />
+      <Infinito slides={cardimg4} />
+
+
+      {/* <Carrusel slides={diapositivas} title='Carusel React' /> */}
 
 
     </>
--- src/lib/constants/card.imgs.ts
+++ src/lib/constants/card.imgs.ts
@@ -13,8 +13,15 @@ import img10 from "../../assets/Group49.svg";
 import img11 from "../../assets/Group51.svg";
 import img12 from "../../assets/Group52.svg";
 
+import img13 from "../../assets/Maskgroup.svg";
+import img14 from "../../assets/pexels.svg";
+import img15 from "../../assets/daniel.svg";
+import img16 from "../../assets/michael.svg";
+
 const cardimg1: string[] = [img1, img2, img3, img4];
 const cardimg2: string[] = [img5, img6, img7, img8];
 const cardimg3: string[] = [img9, img10, img11, img12];
+const cardimg4: string[] = [img13, img14, img15, img16];
 
-export { cardimg1, cardimg2, cardimg3 };
+
+export { cardimg1, cardimg2, cardimg3, cardimg4 };
